57bcebb4b096b74ab1212bf154bd18c23f84c5be
Damary Meliza <106458273+damarymejia@users.noreply.github.com>
2026-04-14 14:13:26 -0600

    Exclude DataSourceAutoConfiguration in Spring Boot app

--- backend/src/main/java/tienda/BackendApplication.java
+++ backend/src/main/java/tienda/BackendApplication.java
@@ -2,8 +2,9 @@ package com.tiendahn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration; // Importación necesaria
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class BackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
